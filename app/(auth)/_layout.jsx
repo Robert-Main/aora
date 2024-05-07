1f07f502fb232d1fbdb218efe9a8ec0c78d051fa
Robert-Main <mainrobert04@gmail.com>
2024-05-07 12:55:01 +0300

    set-up

---
+++ app/(auth)/_layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Authlayout = () => {
+    return (
+        <View>
+            <Text>Authlayout</Text>
+        </View>
+    );
+};
+
+export default Authlayout;
+
+const styles = StyleSheet.create({});
---
+++ app/(tabs)/bookmark.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const BookMark = () => {
+    return (
+        <View>
+            <Text>BookMark</Text>
+        </View>
+    );
+};
+
+export default BookMark;
+
+const styles = StyleSheet.create({});
