5081c67cd63d311c2e05ae6ad6cf45c6b3472cc7
Robert-Main <mainrobert04@gmail.com>
2024-05-08 19:19:05 +0300

    adding the file implemetation

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,14 +1,28 @@
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 
 const Authlayout = () => {
-    return (
-        <View>
-            <Text>Authlayout</Text>
-        </View>
-    );
+  return (
+    <>
+      <Stack>
+        <Stack.Screen
+          name="sign-in"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="sign-up"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+
+      <StatusBar backgroundColor="#161622" style="light" />
+    </>
+  );
 };
 
 export default Authlayout;
-
-const styles = StyleSheet.create({});
--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,14 +1,82 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, ScrollView, Image } from "react-native";
+
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import { useState } from "react";
+import CustomButton from "../../components/CustomButton";
+import { Link } from "expo-router";
 
 const SignUp = () => {
-    return (
-        <View>
-            <Text>SignUp</Text>
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const submit = () => {
+    console.log(form);
+  };
+  return (
+    <SafeAreaView className="flex items-center justify-center h-full bg-primary">
+      <ScrollView>
+        <View
+          className="flex items-center !justify-center w-full h-full px-4 my-6"
+          style={{}}
+        >
+          <Image
+            source={images.logo}
+            resizeMode="contain"
+            className="w-[115px] h-[34px]"
+          />
+
+          <Text className="mt-10 text-2xl font-semibold text-white font-psemibold">
+            Sihg up to Aora
+          </Text>
+
+          <FormField
+            title="Username"
+            value={form.username}
+            handleChangeText={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
+          />
+
+          <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+
+          <FormField
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton
+            handlePress={submit}
+            containerStyles="mt-7 w-full"
+            isLoading={isSubmitting}
+            title="Sign up"
+          />
+
+          <View className="flex flex-row justify-center gap-2 pt-5">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Have an account already?
+            </Text>
+            <Link
+              href="/sign-in"
+              className="text-lg font-psemibold text-secondary"
+            >
+              Signin
+            </Link>
+          </View>
         </View>
-    );
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default SignUp;
-
-const styles = StyleSheet.create({});
